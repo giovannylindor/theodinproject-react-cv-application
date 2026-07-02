e566e50ffa55054f111dd55c85bf523262cdb2be
giovannylindor <giovannylindor56@gmail.com>
2026-07-01 20:41:49 -0400

    reverted back to restart.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
-import Form from './components/Form'
 
 function App() {
     return(
         <>
-            <Form />        
+            <h1>Hello, World!</h1>        
         </>
     );
 }
