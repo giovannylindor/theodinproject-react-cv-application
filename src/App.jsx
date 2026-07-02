d4edb8890e258f74428b2e005b4586ae2ea4d29a
giovannylindor <giovannylindor56@gmail.com>
2026-07-02 01:01:01 -0400

    Completed CSS styling and basic version + added some shadcn inspired styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Form from './components/Form';
+
 function App() {
     return(
         <>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import '../styles/Form.css'
 
 function RenderedForm({infoData, setIsSubmitted}){
     const editForm = () => {
@@ -8,25 +8,28 @@ function RenderedForm({infoData, setIsSubmitted}){
 
     return(
         <>
+        <div className="resumeContainer">
             <h1>{infoData.name}</h1>
-            <p>{infoData.email}</p>
-            <p>{infoData.phoneNumber}</p>
-            <hr />
+            <div className="emailNumber">
+                <p>{infoData.email}</p>
+                <p>{infoData.phoneNumber}</p>
+            </div>
 
+            <h2>Education</h2>
             <p>{infoData.schoolName}</p>
             <p>Major: {infoData.major}</p>
             <p>Date Started: {infoData.dateOfStudy}</p>
-            <hr />
+            
 
             <h2>Work Experience</h2>
             <h3>{infoData.companyName}</h3>
             <p>{infoData.positionTitle}</p>
             <p>{infoData.dateStarted} - {infoData.dateEnded}</p>
             <p>{infoData.jobDescription}</p>
-            <br />
-            <br />
 
             <button onClick={editForm}>Edit Form</button>
+        </div>
+
         </>
     );
 }
@@ -82,72 +85,75 @@ export default function Form(){
 
     return(
         <>
-            <form onSubmit={handleFormSubmit}>
-                {/* Worry about grouping later */}
-                <div>
-                    <label htmlFor="">First Name </label>
-                    <input type="text" required placeholder="John" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-                </div>
+            <form onSubmit={handleFormSubmit} className="formComponent">
+                <div className="formContents">
+                    <div className="contentSection">
+                        <label htmlFor="">First Name </label>
+                        <input type="text" required placeholder="John" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Last Name </label>
-                    <input type="text" required placeholder="Doe"value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Last Name </label>
+                        <input type="text" required placeholder="Doe"value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Email </label>
-                    <input type="email" required placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Email </label>
+                        <input type="email" required placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Phone Number </label>
-                    <input type="tel" required maxLength={10} placeholder="123-456-7890" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Phone Number </label>
+                        <input type="tel" required maxLength={10} placeholder="123-456-7890" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">School Name </label>
-                    <input type="text" required placeholder="Brooklyn College" value={schoolName} onChange={(e) => setSchoolName(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">School Name </label>
+                        <input type="text" required placeholder="Brooklyn College" value={schoolName} onChange={(e) => setSchoolName(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Major of Study </label>
-                    <input type="text" required placeholder="Computer Science" value={major} onChange={(e) => setMajor(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Major of Study </label>
+                        <input type="text" required placeholder="Computer Science" value={major} onChange={(e) => setMajor(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Date of Study </label>
-                    <input type="date" required value={dateOfStudy} onChange={(e) => setDateOfStudy(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Date of Study </label>
+                        <input type="date" required value={dateOfStudy} onChange={(e) => setDateOfStudy(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Company Name </label>
-                    <input type="text" required placeholder="Amazon" value={companyName} onChange={(e) => setCompanyName(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Company Name </label>
+                        <input type="text" required placeholder="Amazon" value={companyName} onChange={(e) => setCompanyName(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Position Title </label>
-                    <input type="text" required placeholder="Junior Software Engineer" value={positionTitle} onChange={(e) => setPositionTitle(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Position Title </label>
+                        <input type="text" required placeholder="Junior Software Engineer" value={positionTitle} onChange={(e) => setPositionTitle(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Job Description </label>
-                    <input type="text" required placeholder="Improve API Structure" value={jobDescription} onChange={(e) => setJobDescription(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Job Description </label>
+                        <textarea name="" id="" cols="42" rows="5" value={jobDescription} onChange={(e) => setJobDescription(e.target.value)}></textarea>
+                    </div>
 
-                <div>
-                    <label htmlFor="">Date Started </label>
-                    <input type="date" required value={dateStarted} onChange={(e) => setDateStarted(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Date Started </label>
+                        <input type="date" required value={dateStarted} onChange={(e) => setDateStarted(e.target.value)}/>
+                    </div>
 
 
-                <div>
-                    <label htmlFor="">Date Ended </label>
-                    <input type="date" required value={dateEnded} onChange={(e) => setDateEnded(e.target.value)}/>
-                </div>
+                    <div className="contentSection">
+                        <label htmlFor="">Date Ended </label>
+                        <input type="date" required value={dateEnded} onChange={(e) => setDateEnded(e.target.value)}/>
+                    </div>
+    
 
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
+                    <div className="submitButton">
+                        <button type="submit">Submit</button>
+                    </div>                
+                </div>                
+
             </form>
         </>
     );
